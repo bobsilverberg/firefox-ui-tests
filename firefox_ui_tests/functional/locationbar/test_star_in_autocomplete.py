d7d4c2be95011bbcc8242173c7cf521bccf1d2c1
Bob Silverberg <bob.silverberg@gmail.com>
2015-02-09 11:33:49 -0500

    Address review comments
    Use self.test_urls
    Remove clean_up_state() method
    Remove except
    Use places.wait_for_visited()
    Use By.ID in place of 'id'
    Place expected value second in comparison

--- firefox_ui_tests/functional/locationbar/test_star_in_autocomplete.py
+++ firefox_ui_tests/functional/locationbar/test_star_in_autocomplete.py
@@ -2,9 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import time
-
-from marionette.errors import NoSuchElementException
+from marionette import By
 
 from firefox_ui_harness.decorators import skip_under_xvfb
 from firefox_ui_harness.testcase import FirefoxTestCase
@@ -21,49 +19,48 @@ class TestStarInAutocomplete(FirefoxTestCase):
     def setUp(self):
         FirefoxTestCase.setUp(self)
 
-        self.clean_up_state()
+        self.test_urls = [self.marionette.absolute_url('layout/mozilla_grants.html')]
 
         # Location bar suggests 'History and Bookmarks'
         self.prefs.set_pref(self.PREF_LOCATION_BAR_SUGGEST, 0)
+
+        with self.marionette.using_context('content'):
+            self.marionette.navigate('about:blank')
+
+        self.places.remove_all_history()
 
     def tearDown(self):
         # Close the autocomplete results
         try:
             self.browser.navbar.locationbar.autocomplete_results.close()
             self.places.restore_default_bookmarks()
-        except NoSuchElementException:
-            pass
         finally:
             FirefoxTestCase.tearDown(self)
-
-    def clean_up_state(self):
-        with self.marionette.using_context('content'):
-            self.marionette.navigate('about:blank')
-
-        self.places.remove_all_history()
 
     @skip_under_xvfb
     def test_star_in_autocomplete(self):
         search_string = 'grants'
 
+        def visit_urls():
+            for url in self.test_urls:
+                self.marionette.navigate(url)
+
         with self.marionette.using_context('content'):
-            self.marionette.navigate(self.marionette.absolute_url('layout/mozilla_grants.html'))
+            self.places.wait_for_visited(self.test_urls, visit_urls)
 
         # TODO: Convert to l10n friendly accessor when menu library is available
         self.browser.menubar.select('Bookmarks', 'Bookmark This Page')
         # TODO: Replace hard-coded selector with library method when one is available
-        done_button = self.marionette.find_element('id', 'editBookmarkPanelDoneButton')
+        done_button = self.marionette.find_element(By.ID, 'editBookmarkPanelDoneButton')
         self.wait_for_condition(lambda mn: done_button.is_displayed)
         done_button.click()
 
-        self.clean_up_state()
+        with self.marionette.using_context('content'):
+            self.marionette.navigate('about:blank')
+
+        self.places.remove_all_history()
 
         locationbar = self.browser.navbar.locationbar
-        # TODO: Replace with places.wait_for_visited when observers have been implemented
-        # https://bugzilla.mozilla.org/show_bug.cgi?id=1121691
-        # The data for autocomplete does not load immediately and we cannot wait for an observer
-        # until the bug 1121731 is complete
-        time.sleep(4)
         locationbar.clear()
         locationbar.urlbar.send_keys(search_string)
         autocomplete_results = locationbar.autocomplete_results
@@ -75,7 +72,7 @@ class TestStarInAutocomplete(FirefoxTestCase):
         first_result = autocomplete_results.visible_results[0]
         matching_titles = autocomplete_results.get_matching_text(first_result, 'title')
         for title in matching_titles:
-            self.wait_for_condition(lambda mn: search_string.lower() == title.lower())
+            self.wait_for_condition(lambda mn: title.lower() == search_string)
         self.assertIn('bookmark',
                       first_result.get_attribute('type'),
                       'The auto-complete result is a bookmark')
