dbc2c71ef903ba80b05ce38affa31df346cd53c7
Bob Silverberg <bob.silverberg@gmail.com>
2015-02-12 16:14:46 -0500

    Address review comments
    Rename test file to test_suggest_bookmarks.py
    Move `with self.marionette.using_context('content'):` into the callback
    Add more comments
    Resolve merge conflicts

--- firefox_ui_tests/functional/locationbar/test_suggest_bookmarks.py
+++ firefox_ui_tests/functional/locationbar/test_suggest_bookmarks.py
@@ -42,12 +42,15 @@ class TestStarInAutocomplete(FirefoxTestCase):
         search_string = 'grants'
 
         def visit_urls():
-            for url in self.test_urls:
-                self.marionette.navigate(url)
+            with self.marionette.using_context('content'):
+                for url in self.test_urls:
+                    self.marionette.navigate(url)
 
-        with self.marionette.using_context('content'):
-            self.places.wait_for_visited(self.test_urls, visit_urls)
+        # Navigate to all the urls specified in self.test_urls and wait for them to
+        # be registered as visited
+        self.places.wait_for_visited(self.test_urls, visit_urls)
 
+        # Bookmark the current page using the bookmark menu
         # TODO: Convert to l10n friendly accessor when menu library is available
         self.browser.menubar.select('Bookmarks', 'Bookmark This Page')
         # TODO: Replace hard-coded selector with library method when one is available
@@ -55,24 +58,30 @@ class TestStarInAutocomplete(FirefoxTestCase):
         self.wait_for_condition(lambda mn: done_button.is_displayed)
         done_button.click()
 
+        # We must open the blank page so the autocomplete result isn't "Switch to tab"
         with self.marionette.using_context('content'):
             self.marionette.navigate('about:blank')
 
         self.places.remove_all_history()
 
+        # Focus the locationbar, delete any contents there, and type the search string
         locationbar = self.browser.navbar.locationbar
         locationbar.clear()
         locationbar.urlbar.send_keys(search_string)
         autocomplete_results = locationbar.autocomplete_results
 
+        # Wait for the search string to be present, for the autocomplete results to appear
+        # and for there to be exactly one autocomplete result
         self.wait_for_condition(lambda mn: locationbar.value == search_string)
         self.wait_for_condition(lambda mn: autocomplete_results.is_open)
         self.wait_for_condition(lambda mn: len(autocomplete_results.visible_results) == 1)
 
+        # Compare the highlighted text in the autocomplete result to the search string
         first_result = autocomplete_results.visible_results[0]
         matching_titles = autocomplete_results.get_matching_text(first_result, 'title')
         for title in matching_titles:
             self.wait_for_condition(lambda mn: title.lower() == search_string)
+
         self.assertIn('bookmark',
                       first_result.get_attribute('type'),
                       'The auto-complete result is a bookmark')
